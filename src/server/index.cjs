3aed121d39bbd2a86ddb8fc5ed6bc51b7db31042
Fatwa <fatwaaryasatyaakbar@gmail.com>
2023-02-24 11:37:22 +0700

    node scheduler

--- src/server/index.cjs
+++ src/server/index.cjs
@@ -4,6 +4,9 @@ const siswaRouter = require("./routers/siswa.cjs");
 const redisRouter = require("./routers/redis.cjs");
 const cors = require("cors");
 const app = express();
+const schedule = require("node-schedule");
+const Log = require("./models/index.cjs").log;
+const uuid = require("uuid");
 require("./db/mongoose.cjs");
 
 const port = 3000;
@@ -14,6 +17,19 @@ app.use(cors());
 app.use("/api/v1/mongoose", studentRouter);
 app.use("/api/v1/sequelize", siswaRouter);
 app.use("/api/v1/redis", redisRouter);
+
+//schedule job every 5 minute
+const job = schedule.scheduleJob("*/5 * * * *", async () => {
+  const data = {
+    msg: uuid.v4(),
+    waktu_sekarang: new Date(),
+  };
+  await Log.create(data);
+
+  console.log("job running");
+});
+
+job.invoke();
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
